9634e12714ad7189a1b765e1f7e1802407ace451
Cedric <cedric.michel@outlook.be>
2021-12-30 15:07:53 +0100

    remove startup => use Top-level statements https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-9#top-level-statements

--- EshopOnVue.js/EshopOnVue.js.Spa/Program.cs
+++ EshopOnVue.js/EshopOnVue.js.Spa/Program.cs
@@ -1,32 +1,86 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using EshopOnVue.js.Infrastructure.Data;
+using MediatR;
+using System.Reflection;
+using VueCliMiddleware;
 
-namespace EshopOnVue.js.Spa
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddSpaStaticFiles(configuration =>
 {
-    /// <summary>
-    /// Entry point
-    /// </summary>
-    public class Program
-    {
-        /// <summary>
-        /// Main method in the entry point to start the application
-        /// </summary>
-        /// <param name="args"></param>
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    configuration.RootPath = "ClientApp/dist";
+});
 
-        /// <summary>
-        /// Creation of the hosting
-        /// </summary>
-        /// <param name="args"></param>
-        /// <returns></returns>
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo
+    {
+        Title = "Eshop on web",
+        Version = "v1"
+    });
+
+    // Set the comments path for the Swagger JSON and UI.
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
+EshopOnVue.js.Infrastructure.Dependencies.ConfigureServices(builder.Configuration, builder.Services);
+
+builder.Services.AddMediatR(typeof(Program));
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseRouting();
+app.UseSpaStaticFiles();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
+
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Eshop on web Api V1");
+});
+
+var seedOnStartup = false;
+if (builder.Configuration["SeedOnStartup"] != null)
+{
+    seedOnStartup = bool.Parse(builder.Configuration["SeedOnStartup"]);
+}
+if (seedOnStartup)
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var context = scope.ServiceProvider.GetRequiredService<EshopContext>();
+        CatalogItemSeed.Seed(context);
     }
 }
+
+app.UseSpa(spa =>
+{
+    if (app.Environment.IsDevelopment())
+        spa.Options.SourcePath = "ClientApp/";
+    else
+        spa.Options.SourcePath = "ClientApp/dist";
+
+    if (app.Environment.IsDevelopment())
+    {
+        spa.UseVueCli(npmScript: "serve");
+    }
+
+});
+
+app.Run();
+
+public partial class Program { }
