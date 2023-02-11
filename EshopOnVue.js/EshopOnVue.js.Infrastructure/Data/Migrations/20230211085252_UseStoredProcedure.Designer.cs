18611254bfb6a15186fb260174e801b1958cb179
cedricmichel <cedric.michel@outlook.be>
2023-02-11 09:55:10 +0100

    add empty migration

--- EshopOnVue.js/EshopOnVue.js.Infrastructure/Data/Migrations/20230211085252_UseStoredProcedure.Designer.cs
+++ EshopOnVue.js/EshopOnVue.js.Infrastructure/Data/Migrations/20230211085252_UseStoredProcedure.Designer.cs
@@ -4,6 +4,7 @@ using EshopOnVue.js.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EshopOnVue.js.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(EshopContext))]
-    partial class EshopContextModelSnapshot : ModelSnapshot
+    [Migration("20230211085252_UseStoredProcedure")]
+    partial class UseStoredProcedure
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
